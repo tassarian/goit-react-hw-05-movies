3edb08857f95c157d4aa3b45bd258827d6f58aa2
tassarian <122233433+tassarian@users.noreply.github.com>
2023-06-09 17:52:32 +0300

    fix

--- src/pages/MovieSubPage/MovieSubPage.jsx
+++ src/pages/MovieSubPage/MovieSubPage.jsx
@@ -9,50 +9,58 @@ const MovieSubPage = () => {
 	const navigate = useNavigate();
 	const genres = movie.genres;
 	console.log(genres);
-	return (
-		<>
-			<button
-				onClick={() => {
-					navigate(location?.state?.from);
-				}}
-			>
-				Go Back
-			</button>
-			<StyledWrapper>
-				<img
-					src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
-					alt="movie-poster"
-					width="400"
-				/>
+	if (movie && genres?.length) {
+		return (
+			<>
+				<button
+					onClick={() => {
+						navigate(location?.state?.from);
+					}}
+				>
+					Go Back
+				</button>
+				<StyledWrapper>
+					<img
+						src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
+						alt="movie-poster"
+						width="400"
+					/>
+					<div>
+						<h2>{movie.title}</h2>
+						<p>{`User score: ${movie.vote_average}%`}</p>
+						<h3>Overview</h3>
+						<p>{movie.overview}</p>
+						<h3>Genres</h3>
+						<ul>
+							{genres.map(el => {
+								return (
+									<li key={el.id}>
+										<p>{el.name}</p>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</StyledWrapper>
 				<div>
-					<h2>{movie.title}</h2>
-					<p>{`User score: ${movie.vote_average}%`}</p>
-					<h3>Overview</h3>
-					<p>{movie.overview}</p>
-					<h3>Genres</h3>
-			
-						{/* <GenresList/> */}
-				
+					<h4>Additional information</h4>
+					<StyledUl>
+						<li>
+							<Link to="cast" state={location.state}>
+								Cast
+							</Link>
+						</li>
+						<li>
+							<Link to="reviews" state={location.state}>
+								Reviews
+							</Link>
+						</li>
+					</StyledUl>
 				</div>
-			</StyledWrapper>
-			<div>
-				<h4>Additional information</h4>
-				<StyledUl>
-					<li>
-						<Link to="cast" state={location.state}>
-							Cast
-						</Link>
-					</li>
-					<li>
-						<Link to="reviews" state={location.state}>
-							Reviews
-						</Link>
-					</li>
-				</StyledUl>
-			</div>
-			<Outlet />
-		</>
-	);
+				<Outlet />
+			</>
+		);
+	}
 };
 
 export default MovieSubPage;
